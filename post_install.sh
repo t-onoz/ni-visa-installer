88e819f7f0f4ea96e969cd0be3dc9b25991f7636
t-onoz <18749921+t-onoz@users.noreply.github.com>
2018-01-28 18:16:14 +0900

    Use patches & modified nikal.c for newer kernels

--- post_install.sh
+++ post_install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 register() {
-    install -b -m755 ./init.d/nipal /usr/local/natinst/nipal/etc/init.d/nipal
+    touch /sbin/chkconfig
+    chmod +x /sbin/chkconfig
+    patch -uNb /usr/local/natinst/nikal/src/nikal/nikal.c < ./nikal.c.patch
+    patch -uNb /usr/local/natinst/nipal/etc/init.d/nipal < ./nipal.patch
+    rm -rf /var/lib/nikal
     install -b -m644 ./menus/*.menu /etc/xdg/menus/applications-merged/
     install -b -m644 ./services/*.service /etc/systemd/system/
     chmod a-x /etc/init.d/*
     chmod +x /usr/local/natinst/nipal/etc/init.d/*
     systemctl daemon-reload
     systemctl enable nipal && echo "Enabled nipal.service"
+    /usr/local/bin/updateNIDrivers --no-prompt
 }
 
 register && cat << EOF
